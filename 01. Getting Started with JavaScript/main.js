58744cf3ece45c1008712c1aa491c3a2487b7ac0
ariverapaniza <ariverapaniza@gmail.com>
2021-08-01 07:42:38 -0500

    Updated Commit

---
+++ 01. Getting Started with JavaScript/main.js
@@ -0,0 +1,5 @@
+document.write("Hello World!"); // This will write this on the HTML //
+console.log("This is the main.js file!");
+document.write("Welcome to this exercise!"); // This will write this on the HTML //
+console.log("It Worked!");
+alert("Welcome!")
